8e2ba0e101b0e09d51fb2ad94b7f2d91f30f4d5f
Timo Kaufmann <timokau@zoho.com>
2020-01-21 10:34:09 +0100

    Move utility functions out of the cli

--- nix_bisect/git.py
+++ nix_bisect/git.py
@@ -2,6 +2,7 @@
 
 from subprocess import run, PIPE
 from math import log, floor, ceil
+import signal
 
 
 def cur_commit():
@@ -54,3 +55,129 @@ def bisect_status():
     return "Bisecting: {} revisions left to test after this (roughly {} steps).".format(
         ceil((bisect_revisions() - 1) / 2), bisect_steps_remaining() - 1,
     )
+
+
+class assure_nothing_unstaged:
+    """Context that temporarily commits staged changes."""
+
+    def __enter__(self):
+        self.head_before = cur_commit()
+        add(".")
+        commit(f"TMP clean slate")
+        return None
+
+    def __exit__(self, type, value, traceback):
+        s = signal.signal(signal.SIGINT, signal.SIG_IGN)
+        reset(self.head_before)
+        signal.signal(signal.SIGINT, s)
+
+
+class git_checkpoint:
+    """Context that remembers the repository's state
+
+    The repositories state (including uncommited changes) is saved when the
+    context is entered and restored when it is left.
+    """
+
+    def __enter__(self):
+        self.head_before = cur_commit()
+
+        # Create a commit that reflects the current state of the repo.
+        add(".")
+        commit(f"TMP clean slate")
+        self.checkpint_rev = cur_commit()
+
+        # Return to the original commit (soft reset).
+        reset(self.head_before)
+        return None
+
+    def __exit__(self, type, value, traceback):
+        # Don't exit in the middle of cleanup, can happen if someone is
+        # impatient and hits ctrl-c multiple times.
+        s = signal.signal(signal.SIGINT, signal.SIG_IGN)
+        reset(self.checkpint_rev, extra_flags=["--hard"])
+        clean(extra_flags=["-f"])
+        reset(self.head_before)
+        signal.signal(signal.SIGINT, s)
+
+
+def try_cherry_pick(rev):
+    with assure_nothing_unstaged():
+        result = run(
+            ["git", "cherry-pick", "-n", rev],
+            stdout=PIPE,
+            stderr=PIPE,
+            encoding="utf-8",
+        )
+
+        if result.returncode != 0:
+            print("Cherry-pick failed")
+            print(result.stderr.splitlines()[0][len("error: ") - 1 :])
+            reset("HEAD", extra_flags=["--hard"])
+            return False
+
+        print("Cherry-pick succeeded")
+        return True
+
+
+def try_revert(rev):
+    with assure_nothing_unstaged():
+        result = run(
+            ["git", "revert", "-n", rev], stdout=PIPE, stderr=PIPE, encoding="utf-8",
+        )
+
+        if result.returncode != 0:
+            print("Revert failed")
+            print(result.stderr.splitlines()[0][len("error: ") - 1 :])
+            reset("HEAD", extra_flags=["--hard"])
+            return False
+
+        print("Revert succeeded")
+        return True
+
+
+def is_ancestor(rev, ancestor_from="HEAD"):
+    result = run(
+        ["git", "merge-base", "--is-ancestor", rev, ancestor_from],
+        stdout=PIPE,
+        stderr=PIPE,
+        encoding="utf-8",
+    )
+
+    if result.returncode == 0:
+        return True
+    if result.returncode == 1:
+        return False
+    result.check_returncode()
+
+
+def reset(rev, extra_flags=[]):
+    result = run(
+        ["git", "reset"] + extra_flags + [rev],
+        stdout=PIPE,
+        stderr=PIPE,
+        encoding="utf-8",
+    )
+    result.check_returncode()
+
+
+def clean(extra_flags=[]):
+    result = run(
+        ["git", "clean"] + extra_flags, stdout=PIPE, stderr=PIPE, encoding="utf-8",
+    )
+    result.check_returncode()
+
+
+def add(path):
+    result = run(["git", "add", path], stdout=PIPE, stderr=PIPE, encoding="utf-8",)
+    result.check_returncode()
+
+
+def commit(message):
+    result = run(
+        ["git", "commit", "--allow-empty", "-m", message],
+        stdout=PIPE,
+        stderr=PIPE,
+        encoding="utf-8",
+    )
+    result.check_returncode()
