402f8a1f49a7af9eff208cd0663654c0cac0e439
Timo Kaufmann <timokau@zoho.com>
2020-03-19 12:29:28 +0100

    Add default.nix
    The CLI can now be tested with a simple
    `nix-shell --run 'nix-bisect -f /path/to/nixpkgs attrname'`.

---
+++ default.nix
@@ -0,0 +1,23 @@
+let
+  # pkgs = import <nixpkgs> {};
+  # pkgs = import /home/timo/repos/nixpkgs/root {};
+  pkgs = let
+    # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
+    nixpkgs-rev = "ddf87fb1baf8f5022281dad13fb318fa5c17a7c6";
+  in import (builtins.fetchTarball {
+    name = "nixpkgs-${nixpkgs-rev}";
+    url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
+  }) {};
+  inherit (pkgs) lib;
+  nix-bisect = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "nix-bisect";
+    version = "git";
+    src = lib.cleanSource ./.;
+    propagatedBuildInputs = with pkgs.python3.pkgs; [
+      appdirs
+      pexpect
+    ];
+  };
+in
+  # python3.withPackages(ps: with ps; [nix-bisect])
+  nix-bisect
