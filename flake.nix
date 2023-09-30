03de258516fd65808b12fec27ec1bba94dce3931
Nathan Henrie <nate@n8henrie.com>
2023-09-30 06:06:29 -0600

    Add explanatory comment

--- flake.nix
+++ flake.nix
@@ -9,6 +9,13 @@
   }: let
     inherit (nixpkgs) lib;
     systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
+
+    # This function build a closure that maps the above systems to a function
+    # accepting a system and returning an attrset in which that system can be
+    # used as `${system}`, making it easy to provide packages / apps / etc.
+    # that apply symmetrically to each of the above systems without having to
+    # repeat oneself. In other words, `packages.${system}.foo = ...` becomes `{
+    # `packages = { system1.foo` = ...; system2.foo = ... };`
     systemClosure = attrs:
       builtins.foldl' (acc: system:
         lib.recursiveUpdate acc (attrs system)) {}
