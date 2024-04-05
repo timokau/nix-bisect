c0c9a91198b6fbc051741e409690070b6b6f8e2e
Philip Taron <philip.taron@gmail.com>
2024-04-05 12:59:31 -0700

    Remove dependency on nix-systems/default
    Also, remove and simplify the `systemClosure` function.

--- flake.nix
+++ flake.nix
@@ -2,40 +2,23 @@
   description = "Bisect nix builds. Flake maintained by @n8henrie.";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.systems.url = "github:nix-systems/default";
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      systems,
-    }:
+    { self, nixpkgs }:
     let
-      inherit (nixpkgs.lib) foldl' recursiveUpdate;
-
-      # This function build a closure that maps the above systems to a function accepting a system
-      # and returning an attrset in which that system can be used as `${system}`, making it easy to
-      # provide packages, apps, and so forth that apply symmetrically to each of the above systems
-      # without having to repeat oneself.
-      #
-      # In other words, `packages.${system}.foo = ...` becomes:
-      # ```nix
-      # {
-      #   packages = {
-      #     system1.foo = ...;
-      #     system2.foo = ...;
-      #   };
-      # }
-      # ```
-      systemClosure =
-        attrs: foldl' (acc: system: recursiveUpdate acc (attrs system)) { } (import systems);
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      eachSystem = f: nixpkgs.lib.genAttrs systems f;
     in
-    systemClosure (system: {
-      packages.${system} = {
+    {
+      packages = eachSystem (system: {
         default = self.packages.${system}.nix-bisect;
         nix-bisect = nixpkgs.legacyPackages.${system}.python3.pkgs.callPackage ./package.nix { };
-      };
+      });
 
-      apps.${system} = self.packages.${system}.default.passthru.apps;
-    });
+      apps = eachSystem (system: self.packages.${system}.default.passthru.apps);
+    };
 }
